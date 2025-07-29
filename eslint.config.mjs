99e21c7319e894228e12ad4b3832ad94c7cbe095
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-07-30 00:52:45 +0600

    set up completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,15 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default tseslint.config([
   eslint.configs.recommended,
   //   tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
     rules: {
-        "no console" : "error"
+        // "no-console": "error"
+        "no-console": "warn"
     }
   }
-);
+]);
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/modules/config/env";
 
 let server: Server;
-let myAge
 
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://testProject:VrWo76PuOLonowfo@cluster0.wnw5g.mongodb.net/tour-management-backend?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(envVars.DB_URL);
 
     console.log("Connected to DB!!");
 
-    server = app.listen(5000, () => {
-      console.log("Server is listening to port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
