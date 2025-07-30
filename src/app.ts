75a54fea9cb9e70bf7627f29b4af4956cb247861
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-07-31 01:43:23 +0600

    route create organize is done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
-import { UserRoutes } from "./app/modules/user/user.route";
 import cors from "cors"
+import { router } from "./app/routes";
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
-app.use("/api/v1/user", UserRoutes)
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
