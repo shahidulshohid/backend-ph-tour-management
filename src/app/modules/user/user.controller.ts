52b96ae389c258f1f7d0dd6ec416ce3dd00876d0
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-08-02 01:01:12 +0600

    create sendResponse utitlity function create is done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,6 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 // const createUserFunction = async (req: Request, res: Response) => {
 
@@ -25,25 +26,43 @@ import { catchAsync } from "../../utils/catchAsync";
 //     next(err);
 //   }
 // };
-const createUser = catchAsync(async ( req: Request, res: Response, next: NextFunction) => {
-  const user = await UserServices.createUser(req.body);
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
 
-  res.status(httpStatus.CREATED).json({
-    message: "User Created Successfully",
-    user,
-  });
-});
+    // res.status(httpStatus.CREATED).json({
+    //   message: "User Created Successfully",
+    //   user,
+    // });
 
-const getAllUsers = catchAsync (async (req: Request, res: Response, next: NextFunction) => {
-  
-    const users = await UserServices.getAllUsers();
-    res.status(httpStatus.OK).json({
-        success: true,
-        message: "All Users Retrieved Successfully",
-        data: users
-    })
-  
-});
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User Created Successfully",
+      data: user,
+    });
+  }
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await UserServices.getAllUsers();
+
+    // res.status(httpStatus.OK).json({
+    //     success: true,
+    //     message: "All Users Retrieved Successfully",
+    //     data: users
+    // })
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "All Users Retrieved Successfully",
+      data: result.data,
+      meta: result.meta
+    });
+  }
+);
 
 // function => try-catch catch => req-res function
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -13,7 +13,13 @@ const createUser = async (payload: Partial<IUser>) => {
 
 const getAllUsers = async () => {
     const users = await User.find({})
-    return users
+    const totalUser = await User.countDocuments()
+    return {
+        data: users,
+        meta: {
+            total: totalUser
+        }
+    }
 }
 
 export const UserServices = {
