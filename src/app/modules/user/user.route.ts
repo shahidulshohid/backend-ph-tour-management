65468758c56e6613f90e5205557ef297cd69a228
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-08-03 00:57:10 +0600

    zod validation is done

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,23 @@
-import { Router } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
+import { AnyZodObject } from "zod";
 
-const router = Router()
-router.post("/register", UserControllers.createUser)
-router.get("/all-users", UserControllers.getAllUsers)
-export const UserRoutes = router
+const validateRequest = (zodSchema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+
+    req.body = await zodSchema.parseAsync(req.body);
+
+    console.log(req.body);
+
+    // next();
+  };
+
+const router = Router();
+
+router.post(
+  "/register",
+
+  UserControllers.createUser
+);
+router.get("/all-users", UserControllers.getAllUsers);
+export const UserRoutes = router;
